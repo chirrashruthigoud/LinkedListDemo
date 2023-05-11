bf3d8f324ea00f6464db5e8b5e1f709611a8c270
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-11 08:12:30 +0530

    Add : linked list base

---
+++ LinkedListDemo/LinkedListDemo/LinkedlistD.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    public class LinkedlistD
+    {
+        public void LinkedlistMethod()
+        {
+            LinkedList<int> linked = new LinkedList<int>();
+            linked.AddLast(1);
+            linked.AddLast(2);
+            linked.AddLast(3);
+            linked.AddLast(4);
+            linked.AddLast(5);
+            linked.AddLast(6);
+
+            //foreach(int a in linked)
+            //{
+            //    Console.WriteLine($"{a}");
+            //}
+            //addfirst
+            linked.AddFirst(10);
+            linked.AddFirst(20);
+            linked.AddFirst(30);
+            linked.AddFirst(40);
+            Console.WriteLine("count :"  +linked.Count);
+
+            //foreach (int a in linked)
+            //{
+            //    Console.WriteLine($"{a}");
+            //}
+            //clear
+            //linked.Clear();
+
+            linked.Remove(linked.Last);
+            linked.Remove(30);
+            linked.RemoveFirst();
+            linked.RemoveLast();
+            foreach (int a in linked)
+            {
+                Console.WriteLine($"{a}");
+            }
+            Console.WriteLine($"count : "  +linked.Count);
+
+        }
+
+    }
+}
---
+++ LinkedListDemo/LinkedListDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //linked list is the liner datastructure
+            //it consist of group of node in sequence
+            //node consist of two parts
+            //data and address
+            //data:-each node of a linked list can store a data.
+            //address:each node of a linkedlist can take a address of the next node(next)
+
+            LinkedlistD linkedlistD = new LinkedlistD();
+            linkedlistD.LinkedlistMethod();
+            Console.ReadLine();
+
+
+        }
+    }
+}
