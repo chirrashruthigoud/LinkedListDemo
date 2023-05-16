4d18fc20054de84a9449e673b68763214151764c
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-16 10:27:41 +0530

    Add : Stack with Linkedlist

---
+++ LinkedListDemo/LinkedListDemo/StackExampleNode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    internal class StackExampleNode
+    {
+    }
+}
