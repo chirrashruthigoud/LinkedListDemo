5399f88f79bc787dbafce4af1e32c5bc4ac2054d
Shruthi Chirra <122668208+chirrashruthigoud@users.noreply.github.com>
2023-05-21 14:34:53 +0530

    Update StackExampleNode.cs

--- LinkedListDemo/LinkedListDemo/StackExampleNode.cs
+++ LinkedListDemo/LinkedListDemo/StackExampleNode.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace LinkedListDemo
 {
-    internal class StackExampleNode
+    public class StackExampleNode
     {
+        public Node top;
+
+        public void StackMethod()
+        {
+            top = null;
+        }
+        public void PushNode(int value)
+        {
+            Node node = new Node(value);
+            if (top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = top;
+            }
+            top = node;
+            Console.WriteLine("Pushed values :" + value);
+        }
+        public void PeekNode()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            Console.WriteLine("peek value :" + this.top.data);
+        }
+        public void PopNode()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            Console.WriteLine("pop value :" + this.top.data);
+            this.top = this.top;
+        }
+        public void Display()
+        {
+            Node temp = top;
+            while (temp != null)
+            {
+                Console.WriteLine("stack values : " + temp.data);
+                temp = temp.next;
+            }
+
+        }
     }
 }
